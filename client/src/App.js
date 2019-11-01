8d5a1fba901620b2afffb99c312dd8e4d42b7916
Nicole Adams <nicoleadams.boston@gmail.com>
2019-11-01 11:28:12 -0400

    Add axios and PlayerCard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import PlayerCard from './components/PlayerCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component {
+
+    constructor (){
+      super();
+      this.state={
+        player: [],
+      }
+    }
+    
+    componentDidMount() {
+      axios
+        .get('http://localhost:5000/api/players')
+        .then(res => {
+          console.log(res.data)
+          this.setState({
+            player: res.data
+          });
+        })
+        .catch(err => {
+          console.log('The data was not returned', err.response);
+        })
+    }
+
+    render() {
+      return(
+      <>
+        <div>
+        {this.state.player.map(player =>  <PlayerCard
+          key={player.id}
+          rank={`${player.id + 1}`}
+          name={player.name}
+          country={player.country}
+          />
+      )}
+      </div>
+    </>
+      )
+    }
+  }
+
+
 
 export default App;
